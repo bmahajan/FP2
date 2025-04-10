3b968fb97cdb82eefd5b331d77724a07cc447616
bonny <bm.bonnymahajan@gmail.com>
2025-04-09 20:37:20 -0400

    publish help

--- my-svelte-app/svelte.config.js
+++ my-svelte-app/svelte.config.js
@@ -1,16 +1,13 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+	},
+};
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    paths: {
-      // replace 'your-repo-name' with the name of your GitHub repo
-      base: '/FP2'
-    }
-  },
-}
+export default config;
